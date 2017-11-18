29c375a174ce55de6be1c0052db594fb64b27faf
gwuhaolin <gwuhaolin@gmail.com>
2017-11-18 15:21:54 +0800

    fix: 检查 dom 被删除

--- book/index.js
+++ book/index.js
@@ -1,50 +1,62 @@
 var className = 'gitbook-plugin-modal';
 
 require(['gitbook'], function (gitbook) {
+
+
   gitbook.events.bind('page.change', function () {
-    var $bookBody = window.document.getElementsByClassName('book-body')[0];
-
-    function showModal(content, closeable) {
-      if ($bookBody.getElementsByClassName('gitbook-plugin-modal').length > 0) {
-        return;
-      }
-      var div = window.document.createElement('div');
-      div.style.left = $bookBody.offsetLeft + 'px';
-      div.style.width = $bookBody.clientWidth + 'px';
-      div.className = className;
-      div.innerHTML = '<div class="gitbook-plugin-modal-content">' + content + '</div>';
-      $bookBody.appendChild(div);
-      if (closeable) {
-        div.onclick = function () {
-          window.document.body.removeChild(div);
-          div = null;
-        }
-      }
-    }
-
+    // 配置项
     var cfg = gitbook.state.config.pluginsConfig.modal || {};
     var html = cfg.html;
     var closeable = cfg.closeable;
     var excludeUrls = cfg.excludeUrls || [];
 
-    for (var i = 0; i < excludeUrls.length; i++) {
-      var exReg = new RegExp(excludeUrls[i], 'g');
-      if (exReg.test(decodeURI(window.location.href))) {
-        return;
+    var shouldClose = false;
+    var $modal;
+    var $bookBody = window.document.getElementsByClassName('book-body')[0];
+
+    function closeModal() {
+      if ($modal) {
+        $modal.remove();
+        $modal = null;
+      }
+      shouldClose = true;
+    }
+
+    function showModal(content, closeable) {
+      $modal = window.document.createElement('div');
+      $modal.style.left = $bookBody.offsetLeft + 'px';
+      $modal.style.width = $bookBody.clientWidth + 'px';
+      $modal.className = className;
+      $modal.innerHTML = '<div class="gitbook-plugin-modal-content">' + content + '</div>';
+      $bookBody.appendChild($modal);
+      if (closeable) {
+        $modal.onclick = closeModal;
       }
     }
 
     function checkModal() {
-      showModal(cfg.html, closeable);
+      if (shouldClose) {
+        return;
+      }
+      // URL 检查
+      for (var i = 0; i < excludeUrls.length; i++) {
+        var exReg = new RegExp(excludeUrls[i], 'g');
+        if (exReg.test(decodeURI(window.location.href))) {
+          return;
+        }
+      }
+      if ($bookBody.getElementsByClassName('gitbook-plugin-modal').length > 0) {
+        return;
+      }
+      showModal(html, closeable);
     }
 
     checkModal();
 
-    if (!closeable) {
-      $bookBody.addEventListener('scroll', checkModal);
-
-      var $bodyInner = window.document.getElementsByClassName('body-inner')[0];
-      $bodyInner.addEventListener('scroll', checkModal);
-    }
+    // 事件监听检查
+    $bookBody.addEventListener('scroll', checkModal);
+    var $bodyInner = window.document.getElementsByClassName('body-inner')[0];
+    $bodyInner.addEventListener('scroll', checkModal);
+    window.document.getElementById('book-search-input').getElementsByTagName('input')[0].addEventListener('input', closeModal);
   });
 });
